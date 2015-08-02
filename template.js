fe41d60a328c571ae63c2a2bd54c7da8262b3c12
ahmedjalal93 <ahhmedd16@hotmail.com>
2015-08-01 23:24:43 -0600

    Update template.js

--- template.js
+++ template.js
@@ -18,9 +18,10 @@
         } else {
             var clone = template.cloneNode(true);
             var text = clone.innerHTML;
-            var match = text.match(/\{{.*?}\}/g);
-            for (var i = 0; i < match.length; i++) {
-                text = text.replace(match[i], data[match[i].replace(/\W+/g, "")]);
+            var matches = text.match(/\{{.*?}\}/g);
+            for (var i = 0; i < matches.length; i++) {
+                var varname = matches[i].substring(2, matches[i].length - 2);
+                var text = text.replace(matches[i], eval('data.' + varname));
                 clone.innerHTML = text;
                 elem.parentNode.appendChild(clone);
             }
@@ -43,9 +44,10 @@
             }
         } else {
             var text = template.innerHTML;
-            var match = text.match(/\{{.*?}\}/g);
-            for (var i = 0; i < match.length; i++) {
-                var text = text.replace(match[i], data[match[i].replace(/\W+/g, "")]);
+            var matches = text.match(/\{{.*?}\}/g);
+            for (var i = 0; i < matches.length; i++) {
+                var varname = matches[i].substring(2, matches[i].length - 2);
+                var text = text.replace(matches[i], eval('data.' + varname));
                 elem.innerHTML = text;
             }
         }
