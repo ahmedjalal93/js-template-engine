b8f6335d1ffcad50eb5f1bf701cebfa0f40c6abf
ahmedjalal93 <ahhmedd16@hotmail.com>
2015-08-17 22:36:06 -0600

    Added filters feature
    This feature will allow you to add an effect on the output value before rendered to client.
    Note: so far I just added a "replace" filter.

--- template.js
+++ template.js
@@ -1,54 +1,84 @@
-    var elem = window.document.getElementById("mytemplate");
-    var template = elem.cloneNode(true);
-
-
-    function append() {
+var template = {
+    element: null,
+    default: null,
+    prepare: function (templ) {
+        this.element = window.document.getElementById(templ);
+        this.default = this.element.cloneNode(true);
+        return this;
+    },
+    functions: function(varname, x){
+                if(x === undefined){
+                    var newval = data;
+                }else{
+                    var newval = data[x];
+                }
+                if(varname.indexOf("|") > -1){
+                    var filters = varname.split(/\|/g);
+                    var filter = filters[1].replace(/\s+/g, '').split(/\,/g);
+                    return this.Filters(eval('newval.'+filters[0]), filter);
+                }else{
+                    return eval('newval.'+varname);
+                }
+        console.log(value);
+    },
+    Filters: function(value, filter){
+        switch(filter[0]){
+        case 'Replace':
+            return value.replace(eval(filter[1]), eval(filter[2]));
+        }
+    },
+    append: function (data) {
         if (data instanceof Array) {
             for (var x in data) {
-                var clone = template.cloneNode(true);
+                var clone = this.default.cloneNode(true);
                 var text = clone.innerHTML;
                 var matches = text.match(/\{{.*?}\}/g);
                 for (var i = 0; i < matches.length; i++) {
                     var varname = matches[i].substring(2, matches[i].length - 2);
                     var text = text.replace(matches[i], eval('data[x].' + varname));
                     clone.innerHTML = text;
-                    elem.parentNode.appendChild(clone);
+                    this.element.parentNode.appendChild(clone);
                 }
             }
         } else {
-            var clone = template.cloneNode(true);
+            var clone = this.default.cloneNode(true);
             var text = clone.innerHTML;
             var matches = text.match(/\{{.*?}\}/g);
             for (var i = 0; i < matches.length; i++) {
                 var varname = matches[i].substring(2, matches[i].length - 2);
                 var text = text.replace(matches[i], eval('data.' + varname));
                 clone.innerHTML = text;
-                elem.parentNode.appendChild(clone);
+                this.element.parentNode.appendChild(clone);
             }
         }
-    }
+    },
 
-    function render() {
+    render: function (data) {
         if (data instanceof Array) {
             for (var x in data) {
-                var clone = template.cloneNode(true);
+                var clone = this.default.cloneNode(true);
                 var text = clone.innerHTML;
                 var matches = text.match(/\{{.*?}\}/g);
                 for (var i = 0; i < matches.length; i++) {
                     var varname = matches[i].substring(2, matches[i].length - 2);
-                    var text = text.replace(matches[i], eval('data[x].' + varname));
+                    var text = text.replace(matches[i], this.functions(varname, x));
+console.log(text);
                     clone.innerHTML = text;
-                    elem.innerHTML = '';
-                    elem.parentNode.appendChild(clone);
+                    this.element.parentNode.appendChild(clone);
                 }
             }
         } else {
-            var text = template.innerHTML;
+            var text = this.default.innerHTML;
             var matches = text.match(/\{{.*?}\}/g);
             for (var i = 0; i < matches.length; i++) {
                 var varname = matches[i].substring(2, matches[i].length - 2);
-                var text = text.replace(matches[i], eval('data.' + varname));
-                elem.innerHTML = text;
+                var text = text.replace(matches[i], this.functions(varname));
+                this.element.innerHTML = text;
             }
         }
     }
+}
+
+window.onload = function () {
+template.prepare("mytemplate").render(data);
+}
